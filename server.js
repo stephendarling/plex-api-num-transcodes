18873b4668add95c404186c268a1137c3c4252ad
Stephen Darling <stephen.darling@outlook.com>
2018-08-02 15:37:36 -0400

    using direct stats from tautulli instead of calculating

--- server.js
+++ server.js
@@ -7,25 +7,35 @@ const app = express();
 tt_url = `http://${process.env.TT_HOST}:${process.env.TT_PORT}/api/v2?apikey=${process.env.API_KEY}&cmd=get_activity`
 
 app.get('/', function (req, res) {
-  var transcode_count = 0
+  var stats = {
+    lan_badwidth: 0,
+    wan_bandwidth: 0,
+    total_bandwidth: 0,
+    stream_count: 0,
+    stream_count_direct_play: 0,
+    stream_count_direct_stream: 0,
+    stream_count_transcode: 0
+  }
   axios({
     method: 'get',
     url: tt_url,
     timeout: 10000
     })
     .then(function (response) {
-      sessions = response.data['response']['data']['sessions']
-      transcodes = sessions.filter(session => session['stream_video_decision'] == 'transcode')
-      if (transcodes.length > 0) 
-        transcode_count = transcodes.length
+      data = response.data['response']['data']
+      stats.lan_badwidth = data.lan_bandwidth
+      stats.wan_bandwidth = data.wan_bandwidth
+      stats.total_bandwidth = data.total_bandwidth
+      stats.stream_count = data.stream_count
+      stats.stream_count_direct_play = data.stream_count_direct_play
+      stats.stream_count_direct_stream = data.stream_count_direct_stream
+      stats.stream_count_transcode = data.stream_count_transcode
     })
     .catch(function (error) {
-      console.log('No connection')
+      console.log('No response')
     })
     .then(function () {
-      res.send({
-        'num_transcodes': transcode_count
-      })
+      res.send(stats)
     })
 
 });
