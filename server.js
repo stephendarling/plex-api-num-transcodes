ddb2399421a80eab2aa2e835e574eef368c093f7
Stephen Darling <stephen.darling@outlook.com>
2018-08-02 12:43:24 -0400

    switched to axios and set timeout for no connection

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 require('dotenv').config()
 const express = require('express');
-var request = require('request');
+var axios = require('axios')
 const app = express();
+
 
 tt_url = `http://${process.env.TT_HOST}:${process.env.TT_PORT}/api/v2?apikey=${process.env.API_KEY}&cmd=get_activity`
 
 app.get('/', function (req, res) {
-
-  request(tt_url, function (error, response, body) {
-    data = JSON.parse(body)
-    sessions = data['response']['data']['sessions']
-
-    transcodes = sessions.filter(session => session['stream_video_decision'] == 'transcode')
-    if (transcodes.length > 0 )
+  var transcode_count = 0
+  axios({
+    method: 'get',
+    url: tt_url,
+    timeout: 10000
+    })
+    .then(function (response) {
+      sessions = response.data['response']['data']['sessions']
+      transcodes = sessions.filter(session => session['stream_video_decision'] == 'transcode')
+      if (transcodes.length > 0) 
+        transcode_count = transcodes.length
+    })
+    .catch(function (error) {
+      console.log('No connection')
+    })
+    .then(function () {
       res.send({
-        'num_transcodes': transcodes.length
+        'num_transcodes': transcode_count
       })
-    else 
-      res.send({
-        'num_transcodes': 0
-      })
-
-  });
+    })
 
 });
 
